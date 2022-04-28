bd8de7eb37c388c8d88e4532e81e3a2743169343
Guillem Riera <guillem@rieragalm.es>
2022-04-28 10:22:17 +0200

    Splits version into its own file and adds exploratory testing

---
+++ first_rust_api/src/version.rs
@@ -0,0 +1,34 @@
+use std::fmt;
+use serde::Serialize;
+
+#[derive(Debug, Serialize, Clone)]
+pub struct Version {
+    pub(crate) version: String
+}
+
+impl Version {
+    fn new(version: String) -> Self {
+        Version {
+            version
+        }
+    }
+}
+
+impl fmt::Display for Version {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Write strictly the first element into the supplied output
+        // stream: `f`. Returns `fmt::Result` which indicates whether the
+        // operation succeeded or failed. Note that `write!` uses syntax which
+        // is very similar to `println!`.
+        write!(f, "{}", self.version)
+    }
+}
+
+// get version function returns a JSON reply wrapped in an OK Result
+pub async fn get_version() -> Result<impl warp::Reply, warp::Rejection> {
+    let v = Version { version: "1.0.0".parse().unwrap() };
+    Ok(warp::reply::json(
+        &v
+    ))
+}
---
+++ first_rust_api/tests/exploratory.rs
@@ -0,0 +1,7 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn exploratory() {
+        assert_eq!(2+2,4)
+    }
+}
